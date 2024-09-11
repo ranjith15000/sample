f2ae20ffda16eed9de332758da6f66dcec11e253
EC2 Default User <ec2-user@ip-172-31-41-98.ap-south-1.compute.internal>
2024-09-11 03:50:36 +0000

    first commit

---
+++ student_login.java
@@ -0,0 +1,11 @@
+public class stdent_login
+{
+        public static void main(string args[])
+        {
+                system.out.println("vansro technologies");
+                system.out.println("student login page");
+                system.out.println("enter your user id:");
+                system.out.println("enter your password:");
+        }
+}
+
